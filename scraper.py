ccadf6d2058f9d74ca0a7ef1cf044533aa70d260
Kokydev <osocoding@gmail.com>
2020-11-02 21:27:40 +0000

    Fix currentscore issue

--- scraper.py
+++ scraper.py
@@ -1,13 +1,30 @@
 import requests
 import json
+import time
 from bs4 import BeautifulSoup as bs
 
-response = requests.get('https://www.sportinglife.com/football/live')
+team = "Leeds United"
+currentscore = 0
 
-json_data=bs(response.content,"html.parser").find("script",id="__NEXT_DATA__").string
+def ScrapeScore():
+    response = requests.get('https://www.sportinglife.com/football/live')
 
-matches=json.loads(json_data)["props"]["pageProps"]["matches"]
-for match in matches:
-    home= match["team_score_a"]
-    away= match["team_score_b"]
-    print("{} {} - {} {}".format(home["team"]["name"], home["score"][0]["score"], away["score"][0]["score"], away["team"]["name"]))
+    json_data=bs(response.content,"html.parser").find("script",id="__NEXT_DATA__").string
+
+    matches=json.loads(json_data)["props"]["pageProps"]["matches"]
+    for match in matches:
+        home= match["team_score_a"]
+        away= match["team_score_b"]
+
+        if home["team"]["name"] == team:
+            oldscore = currentscore
+            currentscore = home["score"][0]["score"]
+            if currentscore != oldscore:
+               # SEND SMS 
+                print("{} {} - {} {}".format(home["team"]["name"], home["score"][0]["score"], away["score"][0]["score"], away["team"]["name"]))
+
+
+    time.sleep(60)
+    ScrapeScore()        
+
+ScrapeScore()
