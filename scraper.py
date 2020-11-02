2db37647e0415eae0d6a328a1a07001a0ef2ce10
Kokydev <osocoding@gmail.com>
2020-11-02 21:17:07 +0000

    working scraper
    thanks to xeriphas#6398

--- scraper.py
+++ scraper.py
@@ -1,16 +1,13 @@
 import requests
-from bs4 import BeautifulSoup
+import json
+from bs4 import BeautifulSoup as bs
 
-r = requests.get('https://www.sportinglife.com/football/live')
+response = requests.get('https://www.sportinglife.com/football/live')
 
-result = BeautifulSoup(r.text, 'html.parser')
-print(result.find('Leeds United'))
+json_data=bs(response.content,"html.parser").find("script",id="__NEXT_DATA__").string
 
-string = 'name":"Leeds United","short_name":"Leeds"},"score":'
-
-#if "leeds" in r.text:
-#    for line in r.text.split('\n'):
-#        if string in line:
-#            print(line)
-#else:
-#    print("leeds not found")#
+matches=json.loads(json_data)["props"]["pageProps"]["matches"]
+for match in matches:
+    home= match["team_score_a"]
+    away= match["team_score_b"]
+    print("{} {} - {} {}".format(home["team"]["name"], home["score"][0]["score"], away["score"][0]["score"], away["team"]["name"]))
