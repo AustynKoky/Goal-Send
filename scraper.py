eeac0512cfbdd627cdded4523312477f8c4212ca
Kokydev <osocoding@gmail.com>
2020-11-03 20:36:53 +0000

    Working and notifiying

--- scraper.py
+++ scraper.py
@@ -13,26 +13,19 @@ def ScrapeScore(currentscore, team):
     for match in matches:
         home= match["team_score_a"]
         away= match["team_score_b"]
-
-        if home["team"]["name"] == team:
+        if home["team"]["name"] == team or away["team"]["name"] == team:
             oldscore = currentscore
-            currentscore = home["score"][0]["score"] #CURRENT SCORE IS CHANGED HERE
+            currentscore = int(home["score"][0]["score"]) + int(away["score"][0]["score"])
             if currentscore != oldscore:
                 print("currentscore: " + str(currentscore) + " old score:" + str(oldscore))
-                print("{} {} - {} {}".format(home["team"]["name"], home["score"][0]["score"], away["score"][0]["score"], away["team"]["name"]))  
-                continue
-        elif away["team"]["name"] == team:
-            oldscore = currentscore
-            currentscore = away["score"][0]["score"] #CURRENT SCORE IS CHANGED HERE
-            if currentscore != oldscore:
-                print("currentscore: " + str(currentscore) + " old score:" + str(oldscore))
-                print("{} {} - {} {}".format(home["team"]["name"], home["score"][0]["score"], away["score"][0]["score"], away["team"]["name"]))  
+                notification = "({}) {} [{}] - {} [{}] ".format(match["clock"], home["team"]["name"], home["score"][0]["score"], away["team"]["name"], away["score"][0]["score"])
+                print(notification) 
+                sms.sendSMS('5IDrV18jaeo-dalDi7VyEEoVaDtQjvW5GBb46kA3DP', '353858158414, 353852704841', 'GoalSend', notification)
                 continue
     return currentscore
 
 currentscore=0
+oldscore=0
 while True:
-    currentscore=ScrapeScore(currentscore, "Arsenal")
-    resp = sms.sendSMS('0NSUV4UG8RI-YvAmaTvH17jQLhwcQRPq7JnTlmz7IO', '353858158414, 353852704841',
-        'Pornhub', 'Chuckles?')
+    currentscore=ScrapeScore(currentscore, "Liverpool")
     time.sleep(5)
